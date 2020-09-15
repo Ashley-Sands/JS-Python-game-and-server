af1c36013357c53bc7dc025f5c3f56db58f0775b
Ashley Sands <ashsands1@amstrike.com>
2020-09-15 22:46:56 +0100

    Added Frame info to send data raw payload

--- python - server/message_objects/send_data_raw_payload.py
+++ python - server/message_objects/send_data_raw_payload.py
@@ -4,7 +4,7 @@ class SendDataRawPayload:
         ready to be set into a send message
     """
 
-    def __init__( self, raw_payload_data, to_message_func, to_sockets=None, ignore_sockets=None ):
+    def __init__( self, raw_payload_data, to_message_func, frame_id, frame_timestamp, to_sockets=None, ignore_sockets=None ):
         """
 
         :param raw_payload_data:    the raw payload data
@@ -17,6 +17,9 @@ class SendDataRawPayload:
 
         self.__to_sockets = to_sockets
         self.__ignore_sockets = ignore_sockets
+
+        self.__frame_id = frame_id
+        self.__frame_timestamp = frame_timestamp
 
         if self.__to_sockets is None:
             self.__ignore_sockets = []
@@ -32,6 +35,11 @@ class SendDataRawPayload:
 
     def get_raw( self ):
         return self.__raw_payload
+
+    @property
+    def frame_info( self ):
+        """ Gets the frame id and frame timestamp (tuple. id, timestamp)"""
+        return self.__frame_id, self.__frame_timestamp
 
     def can_send_to( self, socket ):
         return socket not in self.__ignore_sockets and (len( self.__to_sockets ) == 0 or socket in self.__to_sockets)
