1d0e9cda29e24843cba5a44177f1be15104bb4c1
Ashley Sands <ashsands1@amstrike.com>
2020-09-18 01:28:13 +0100

    improved inputs, so values are only sent when changed

--- js - client/includes/javascript/core/managers/Inputs.js
+++ js - client/includes/javascript/core/managers/Inputs.js
@@ -9,12 +9,15 @@ export class Inputs extends ServerBaseObject
 
         this.canvasElement = canvasElement
 
-        this.keys  = {}
+        this._key_values  = {}
+        this._mouse_values = {}
+
+        // changed values.
+        this.keys  = {}     
         this.mouse = {}
 
         window.onkeydown = this.OnKeyDown.bind(this)
         window.onkeyup   = this.OnKeyUp.bind(this)
-        window.onmousemove = this.OnMouseMove.bind(this)
         window.onmousedown = this.OnMouseDown.bind(this)
         window.onmouseup = this.OnMouseUp.bind(this)
         //canvasElement.onmousedown = this.OnMouseDown
@@ -28,33 +31,60 @@ export class Inputs extends ServerBaseObject
     // ie del is 46 but we can determin the correct code, using event.location
     // see https://keycode.info/
     // TODO: Only update inputs if changed.
+    __OnKey( event, value )
+    {
+        try
+        {
+            if (this._key_values[event.key] == value)
+                return
+        }
+        finally
+        {
+            this._key_values[ event.key ] = value
+        }
+
+        this.keys[ event.key ] = value
+    }
+
     OnKeyDown( event )
     {
         console.log( event.key, ":::", event.keyCode )
-        this.keys[ event.key ] = 1
+        
+        this.__OnKey( event, 1)
+        
     }
 
     OnKeyUp( event )
     {
+        this.__OnKey( event, 0)
+    }
 
-        this.keys[ event.key ] = 0
+    __OnMouse( event, value )
+    {
+        try
+        {
+            if (this._mouse_values[event.button] == value)
+                return
+        }
+        finally
+        {
+            this._mouse_values[ event.button ] = value
+        }
+
+        this.mouse[ event.button ] = value
+        this.mouse["position"] = { x: event.pageX, y: event.pageY }
 
     }
 
     OnMouseDown( event )
     {
         console.log( event.button, event.pageX, event.pageY )
-        this.mouse[ event.button ] = 1
-    }
-
-    OnMouseMove( event )
-    {
-        this.mouse["position"] = { x: event.pageX, y: event.pageY }
+        this.__OnMouse( event, 1 )
     }
 
     OnMouseUp( event )
     {
-        this.mouse[ event.button ] = 0
+        this.__OnMouse( event, 0 )
     }
 
     CollectData( sinceTime=0.0 )
