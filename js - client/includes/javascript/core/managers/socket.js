3ae280d788542156357db4036d4b151d12d92c1c
Ashley Sands <ashsands1@amstrike.com>
2020-09-19 15:35:09 +0100

    improved connection handering

--- js - client/includes/javascript/core/managers/socket.js
+++ js - client/includes/javascript/core/managers/socket.js
@@ -1,4 +1,5 @@
 import { Packet } from "../packets/Packet.js"
+import { GameConsole } from "./gameConsole.js"
 
 export class Socket
 {
@@ -7,12 +8,16 @@ export class Socket
         var protocol = local ? "ws://" : "wss://";
         this.webAddress = `${protocol}${address}:${port}${root}`
 
-        this._hasError = false;
-        this._isConnected = false;
+        this._hasError = false
+        this._connecting = false
+        this._isConnected = false
 
-        this._socket = null;
+        this._socket = null
         
         this._receivedMessages = []
+
+        this.retryTimeout = 5
+        this.attamps = 0
 
     }
 
@@ -20,17 +25,20 @@ export class Socket
 
         if ( !this._isConnected || this._hasError )
         {
+            this._connecting = true
 
-            if (this._socket != null && this._hasError && (!this._socket.CLOSED || this._socket.CLOSING))
+            GameConsole.instance.AddMessage( "Connecting...", "SYSTEM" )
+
+            if (this._socket != null && this._hasError && (!this._socket.CLOSED && !this._socket.CLOSING))
                 this._socket.close()
 
-            this._socket = new WebSocket( this.webAddress );
+            this._socket = new WebSocket( this.webAddress )
             this._socket.binaryType = "arraybuffer"
 
-            this._socket.onopen    =    this.__SocketOpen.bind(this);
-            this._socket.onclose   =    this.__SocketClose.bind(this);
-            this._socket.onmessage =    this.__SocketReceive.bind(this);
-            this._socket.onerror   =    this.__SocketError.bind(this);
+            this._socket.onopen    =    this.__SocketOpen.bind(this)
+            this._socket.onclose   =    this.__SocketClose.bind(this)
+            this._socket.onmessage =    this.__SocketReceive.bind(this)
+            this._socket.onerror   =    this.__SocketError.bind(this)
 
         }
         else
@@ -42,13 +50,16 @@ export class Socket
 
     __SocketOpen( e )
     {
-        console.log("Socket Opened");
+        GameConsole.instance.AddMessage( "Connected", "SYSTEM" )
+
         this._isConnected = true;
+        this.attamps = 0
     }
 
     __SocketClose( e )
     {
-        console.log("Socket Closed");
+        GameConsole.instance.AddMessage( "Connection Closed", "SYSTEM" )
+        this.RetryConnection()
         this._isConnected = false
     }
 
@@ -60,8 +71,27 @@ export class Socket
 
     __SocketError( e )
     {
-        console.log("Socket Error");
-        this._hasError = true;
+
+        if ( this._connecting )
+        {
+            GameConsole.instance.AddMessage( "Unable to connect to server", "SYSTEM", null, GameConsole.MESSAGE_TYPE.ERROR )
+        }
+        else
+        {
+            GameConsole.instance.AddMessage( "Connection error", "SYSTEM", null, GameConsole.MESSAGE_TYPE.ERROR )
+        }
+        
+
+        this._hasError = true
+        this._connecting = false
+    }
+
+    RetryConnection()
+    {
+        this._hasError = false
+        this.attamps++
+        GameConsole.instance.AddMessage( `Retring in ${this.retryTimeout+this.attamps} seconds`, "SYSTEM" )
+        setTimeout( this.connect.bind(this), (this.retryTimeout+this.attamps)*1000 )
     }
 
     /**
