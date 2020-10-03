a41b0847bd70fe6fae5cceeea8bf67388ec0e611
Ashley Sands <ashsands1@amstrike.com>
2020-10-03 18:37:59 +0100

    Added Payload data obj so theres a common data type between send and receive
    Payload data handles both raw and structured data types.
    and is intended to replace send_data_raw_payload as a data type.
    we still need somthink for clients but this is heading in the right drection :)

---
+++ python - server/message_objects/payload_data_objects/payload_data.py
@@ -0,0 +1,42 @@
+
+
+class PayloadData:
+    """ Common class to convert data to and from formatted string (ie JSON) """
+
+    def __init__( self ):
+
+        self._string = None
+        self._structure = None
+
+        self._string_u2d = True
+        self._struct_u2d = True
+
+    def _parse_to_string( self ):
+        """Parses the data structure to raw data string"""
+        raise NotImplementedError
+
+    def _parse_to_structure( self ):
+        """Parses the raw data string to structured data"""
+        raise NotImplementedError
+
+    def set_string( self, data_string ):
+        self._string = data_string
+        self._struct_u2d = False
+
+    def set_structure( self, data_structure ):
+        self._structure = data_structure
+        self._string_u2d = False
+
+    def get_raw_data_string( self ):
+
+        if not self._string_u2d:
+            self._parse_to_string()
+
+        return self._string
+
+    def get_data_structure( self ):
+
+        if not self._struct_u2d:
+            self._parse_to_structure()
+
+        return self._structure
---
+++ python - server/message_objects/payload_data_objects/payload_json_data.py
@@ -0,0 +1,27 @@
+import message_objects.payload_data_objects.payload_data as payload_data
+import json
+import common.DEBUG as DEBUG
+_print = DEBUG.LOGS.print
+
+
+class PayloadJsonData( payload_data.PayloadData ):
+
+    def __init__(self):
+        super().__init__()
+
+    def _parse_to_string( self ):
+
+        try:
+            string = json.dumps( self._structure )
+            self._string_u2d = True
+            self._string = string
+        except Exception as e:
+            _print( "Unable to parse to string", e, message_type=DEBUG.LOGS.MSG_TYPE_ERROR )
+
+    def _parse_to_structure( self ):
+        try:
+            struct = json.dumps( self._structure )
+            self._struct_u2d = True
+            self._structure = struct
+        except Exception as e:
+            _print( "Unable to parse to string", e, message_type=DEBUG.LOGS.MSG_TYPE_ERROR )
